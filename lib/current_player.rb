1e25c3d078a473a78d7677252a8c421a8f0021dc
shivani brijmohan <sbrij001@yahoo.com>
2018-11-21 19:09:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -28,9 +28,6 @@ end
 #use the modulo method to determine if even or odd
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end 
+  turn_count(board) % 2 == 0 ? "X": "O"
+
 end
